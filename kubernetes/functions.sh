b2250a9605f886a8258474562731217c880ba679
Hideki Yamada <hideki9th@gmail.com>
2024-10-16 10:12:48 +0900

    Delete kubernetes/functions.sh

--- kubernetes/functions.sh
+++
@@ -1,9 +0,0 @@
-#######################################################
-## functions
-#Check commands exist
-check_command() {
-  if ! command -v "$1" &> /dev/null; then
-    echo "Error: $1 is not installed." >&2
-    exit 1
-  fi
-}
