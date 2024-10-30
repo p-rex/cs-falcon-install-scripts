c518c1dc65a86c4e2d24452520c18a86abdbddbb
Hideki Yamada <hideki9th@gmail.com>
2024-10-30 17:05:29 +0900

    Update config.sh

--- kubernetes/config.sh
+++ kubernetes/config.sh
@@ -1,19 +1,16 @@
 ##------------------------------------------##
-## CID
+## General configuration
 ##------------------------------------------##
+
+# CID
 export FALCON_CID=<CID_WITH_CHECKSUM>
 
-##------------------------------------------##
-## API Credential
-##------------------------------------------##
-export FALCON_CLIENT_ID=<CLIENT_ID>
+# API Credential
+export FALCON_CLIENT_ID=<CLIENT ID>
 export FALCON_CLIENT_SECRET=<CLIENT_SECRET>
 
 
-##------------------------------------------##
-## Region
-##------------------------------------------##
-
+# Region
 # US-1
 # export FALCON_CLOUD_API=api.crowdstrike.com
 # export FALCON_REGION=us-1
@@ -25,14 +22,12 @@ export FALCON_CLOUD_API=api.us-2.crowdstrike.com
 export FALCON_CONTAINER_REGISTRY=registry.crowdstrike.com
 
 
+
 ##------------------------------------------##
-## Falcon sensor configuration
+## Kubernetes configuration
 ##------------------------------------------##
 
-# set sensor mode. "bpf" or "kernel"
-export FALCON_SENSOR_NODE_BACKEND=bpf
-
-# set falcon sensor tags. For multiple tags, use "\,". e.g., "tag1\,tag2\,tag3"
+# Set sensor grouping tags. For multiple tags, use "\,". e.g., "tag1\,tag2\,tag3"
 export FALCON_SENSOR_GROUPING_TAGS=""
 
 # set true if GKE Autopilot
@@ -40,8 +35,15 @@ export FALCON_GKE_AUTOPILOT=false
 
 
 ##------------------------------------------##
-## IAR configuration
+## KAC configuration
 ##------------------------------------------##
 
+# Set sensor grouping tags. For multiple tags, use "\,". e.g., "tag1\,tag2\,tag3"
+export FALCON_KAC_GROUPING_TAGS="falconkac"
+
+
+##------------------------------------------##
+## IAR configuration
+##------------------------------------------##
 # set cluster name
 export FALCON_K8S_CLUSTER_NAME=kubernetes-cluster
