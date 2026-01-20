ceafc5c03353fa46411fbc3ce398dbdfe74bc963
p-rex <hideki9th@gmail.com>
2026-01-20 14:37:49 +0900

    minor fix

--- ecs-ec2-cloudformation/uninstall_all.sh
+++ ecs-ec2-cloudformation/uninstall_all.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 
+# Download cleanup template
+curl -LO https://raw.githubusercontent.com/CrowdStrike/aws-cloudformation-falcon-sensor-ecs/refs/heads/main/falcon-sensor-ecs-ec2/falcon-ecs-ec2-daemon-cleanup.yaml
+
 # Delete the sensor daemon stack from the cluster.
 aws cloudformation delete-stack \
     --stack-name falcon-ecs-ec2-daemon-$FALCON_ECS_EC2_CLUSTER_NAME
-
-
-# Download cleanup template
-curl -LO https://raw.githubusercontent.com/CrowdStrike/aws-cloudformation-falcon-sensor-ecs/refs/heads/main/falcon-sensor-ecs-ec2/falcon-ecs-ec2-daemon-cleanup.yaml
 
 
 # After the daemon stack is removed, clean up the artifacts.
