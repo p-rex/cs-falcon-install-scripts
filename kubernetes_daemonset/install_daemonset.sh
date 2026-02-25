216a20db30736aa03b790eb55e32e2ddb8493b1d
p-rex <hideki9th@gmail.com>
2026-02-25 16:42:25 +0900

    delete region from image name

--- kubernetes_daemonset/install_daemonset.sh
+++ kubernetes_daemonset/install_daemonset.sh
@@ -20,7 +20,7 @@ export FALCON_IMAGE_PULL_TOKEN=$(curl https://raw.githubusercontent.com/CrowdStr
 # Install falcon sensor
 
 #Set Falcon repository
-export FALCON_IMAGE_REPO=${FALCON_CONTAINER_REGISTRY}/falcon-sensor/${FALCON_REGION}/release/falcon-sensor
+export FALCON_IMAGE_REPO=${FALCON_CONTAINER_REGISTRY}/falcon-sensor/release/falcon-sensor
 
 #Set image tag
 export FALCON_IMAGE_TAG=$(curl https://raw.githubusercontent.com/CrowdStrike/falcon-scripts/main/bash/containers/falcon-container-sensor-pull/falcon-container-sensor-pull.sh -s \
@@ -48,7 +48,7 @@ helm install falcon-sensor crowdstrike/falcon-sensor \
 #######################################################
 # Install KAC
 # Set Falcon repository
-export FALCON_KAC_IMAGE_REPO=${FALCON_CONTAINER_REGISTRY}/falcon-kac/${FALCON_REGION}/release/falcon-kac
+export FALCON_KAC_IMAGE_REPO=${FALCON_CONTAINER_REGISTRY}/falcon-kac/release/falcon-kac
 
 # Set image tag
 export FALCON_KAC_IMAGE_TAG=$(curl https://raw.githubusercontent.com/CrowdStrike/falcon-scripts/main/bash/containers/falcon-container-sensor-pull/falcon-container-sensor-pull.sh -s \
